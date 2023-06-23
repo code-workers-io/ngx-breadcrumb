c32f3f2c2c1e0c6c5f4714f8ca00569e5f0f5c22
Michael Berger <m.gonser@icloud.com>
2023-06-23 12:51:19 +0200

    chore: setup cypress ct in demo

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -1,55 +1,77 @@
-import {Injectable, NgModule} from '@angular/core';
+import { Injectable, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import {
   Breadcrumb,
-  NGX_TRANSLATION_ADAPTER,
-  NgxBreadcrumbModule,
-  NgxBreadcrumbTranslationAdapter
-} from "@code-workers.io/ngx-breadcrumb";
-import {RouterModule, Routes} from "@angular/router";
+  NgxBreadcrumbsComponent,
+  NgxBreadcrumbTranslationAdapter,
+  provideNgxBreadcrumbConfig,
+  provideNgxBreadcrumbTranslationAdapter,
+} from '@code-workers.io/ngx-breadcrumb';
+import {provideRouter, RouterOutlet, Routes} from '@angular/router';
 
-import {OneComponent} from "./one.component";
-import {OneOneComponent} from "./one-one.component";
-import {TwoComponent} from "./two.component";
-import {RootBreadcrumbComponent, RootBreadcrumbComponentModule} from "./root-breadcrumb.component";
-@Injectable({providedIn: 'root'})
+import { OneComponent } from './one.component';
+import { OneOneComponent } from './one-one.component';
+import { TwoComponent } from './two.component';
+import {
+  RootBreadcrumbComponent,
+  RootBreadcrumbComponentModule,
+} from './root-breadcrumb.component';
+@Injectable({ providedIn: 'root' })
 class translator implements NgxBreadcrumbTranslationAdapter {
   translate(key: string | undefined | null): string {
     // here you would actually call your translation service
     return `👉🏻 ${key}`;
   }
-
 }
 
 const routes: Routes = [
-  { path: 'one', component: OneComponent, data: { breadcrumb: 'One' }, children: [
-      { path: 'one', component: OneOneComponent, data: { breadcrumb: 'OneOne' } }
-    ] },
+  {
+    path: 'one',
+    component: OneComponent,
+    data: { breadcrumb: 'One' },
+    children: [
+      {
+        path: 'one',
+        component: OneOneComponent,
+        data: { breadcrumb: 'OneOne' },
+      },
+    ],
+  },
   { path: 'two', component: TwoComponent, data: { breadcrumb: 'Two' } },
-]
+];
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, OneComponent, OneOneComponent, TwoComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routes), NgxBreadcrumbModule.withConfig({
-   /* stickyRoot: [
-      new Breadcrumb('Home', '/'),
-      new Breadcrumb('Home1', '/')
-    ],*/
-    breadcrumbCount: {
-      fixedLead: 5,
-      fixedTail: 5,
-    },
-    stickyRootComponent: {
-      component: RootBreadcrumbComponent,
-      data: {
-        label: 'Home',
-        link: '/'
-      }
-    }
-  }), RootBreadcrumbComponentModule],
-  providers: [{ provide: NGX_TRANSLATION_ADAPTER, useClass: translator}],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    OneComponent,
+    OneOneComponent,
+    TwoComponent,
+  ],
+  imports: [BrowserModule, RootBreadcrumbComponentModule, NgxBreadcrumbsComponent, RouterOutlet],
+  providers: [
+    provideRouter(routes),
+    //provideNgxBreadcrumbTranslationAdapter(translator),
+    provideNgxBreadcrumbConfig({
+      stickyRoot: [
+       new Breadcrumb('Home', '/'),
+       new Breadcrumb('Home1', '/')
+     ],
+      breadcrumbCount: {
+        fixedLead: 5,
+        fixedTail: 5,
+      },
+/*      stickyRootComponent: {
+        component: RootBreadcrumbComponent,
+        data: {
+          label: 'Home',
+          link: '/',
+        },
+      },*/
+    }),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
